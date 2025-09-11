739a5cb3c8fdd7f61bece17d15630b0175b59cda
Kyriaki Kolpetinou <168235864+KyriakiKolpetinou@users.noreply.github.com>
2025-09-12 00:54:38 +0300

    Add PanoramaDataModule for data loading and augmentation

--- training/data.py
+++ training/data.py
@@ -81,3 +81,136 @@ class RandContrastd(RandomizableTransform):
                 data[key] = data[key] * self.factor
                 data[key] = np.clip(data[key], intensity_min, intensity_max)
         return data
+
+
+#data loaders
+
+class PanoramaDataModule(L.LightningDataModule):
+    def __init__(
+        self,
+        data_dir: str,
+        batch_size: int = 8,
+        num_workers: int = 2,
+        train_size: float = 0.7,
+        val_size: float = 0.3,
+        train_patch_size: tuple[int, int, int] = (40, 224, 224),
+    ):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.train_patch_size = train_patch_size
+        self.train_size = train_size
+        self.val_size = val_size
+
+    def get_transforms(self, mode):
+        keys = ["image", "label"] if mode != "test" else ["image"]
+        base_keys = keys.copy()
+        keys.append("filename")
+
+        load_transforms = [
+            LoadNpzd(keys=base_keys),
+            ToTensord(keys=base_keys),
+            SpatialPadd(keys=base_keys, spatial_size=self.train_patch_size),
+        ]
+
+
+        augmentations = [
+            RandCropByLabelClassesd(
+        keys=["image","label"],
+        label_key="label",
+        ratios=[0.2, 0.3, 0.5],   # [bg, pancreas, tumor]
+        spatial_size=self.train_patch_size,
+        num_samples=1,
+               image_key="image",
+               num_classes=3,
+               image_threshold=0,  # no thresholding
+               ),
+            RandRotated(
+                keys=["image", "label"],
+                range_x=(0.5),
+                range_y=(0.5),
+                range_z=(0.5),
+                prob=0.2,
+                keep_size=True,
+                mode=("trilinear", "nearest"),
+            ),
+            RandZoomd(
+                keys=["image", "label"],
+                min_zoom=0.7,
+                max_zoom=1.4,
+                prob=0.2,
+                mode=("trilinear", "nearest"),
+                align_corners=(True, None),
+            ),
+            RandGaussianNoised(keys=["image"], std=0.01, prob=0.15),
+            RandGaussianSmoothd(
+                keys=["image"],
+                sigma_x=(0.5, 1.5),
+                sigma_y=(0.5, 1.5),
+                sigma_z=(0.5, 1.5),
+                prob=0.1,
+            ),
+            RandScaleIntensityd(keys=["image"], factors=0.3, prob=0.15),
+            RandContrastd(keys=["image"], prob=0.15),
+            RandSimulateLowResolutiond(
+                keys=["image"],
+                prob=0.125,
+                downsample_mode="nearest",
+                upsample_mode="trilinear",
+                zoom_range=(0.5, 1),
+            ),
+            GammaAugmentationd(
+                keys=["image"],
+                gamma_range=(0.7, 1.5),
+                prob=0.15,
+                invert_prob=0.15,
+            ),
+            RandFlipd(["image", "label"], spatial_axis=[0], prob=0.5),
+            RandFlipd(["image", "label"], spatial_axis=[1], prob=0.5),
+            RandFlipd(["image", "label"], spatial_axis=[2], prob=0.5),
+        ]
+
+        other_transforms = [
+            CastToTyped(keys=["image", "label"], dtype=(torch.float32, torch.uint8))
+        ] if mode != "test" else [CastToTyped(keys=["image"], dtype=(torch.float32))]
+
+        if mode == "train":
+            return Compose(load_transforms + augmentations + other_transforms)
+        else:
+            return Compose(load_transforms + other_transforms)
+
+    def setup(self, stage=None):
+        image_filepaths = sorted(glob.glob(os.path.join(self.data_dir, "imagesTr", "*")))
+        label_filepaths = sorted(glob.glob(os.path.join(self.data_dir, "labelsTr", "*")))
+
+        data_pairs = list(zip(image_filepaths, label_filepaths))
+        random.shuffle(data_pairs)
+        image_filepaths, label_filepaths = zip(*data_pairs)
+
+        num_samples = len(image_filepaths)
+        train_end = int(num_samples * self.train_size)
+
+        train_files = [
+    {"image": image, "label": label, "filename": os.path.basename(image)}
+    for image, label in zip(image_filepaths[:train_end], label_filepaths[:train_end])
+]
+
+        val_files = [
+    {"image": image, "label": label, "filename": os.path.basename(image)}
+    for image, label in zip(image_filepaths[train_end:], label_filepaths[train_end:])
+]
+
+
+        self.train_ds = Dataset(data=train_files, transform=self.get_transforms("train"))
+        self.val_ds = Dataset(data=val_files, transform=self.get_transforms("val"))
+
+    def train_dataloader(self):
+        return DataLoader(self.train_ds, batch_size=self.batch_size, shuffle=True,
+                          num_workers=self.num_workers, drop_last=True,
+                          collate_fn=pad_list_data_collate, pin_memory=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_ds, batch_size=self.batch_size, shuffle=False,
+                          num_workers=self.num_workers,
+                          collate_fn=pad_list_data_collate, pin_memory=True)
